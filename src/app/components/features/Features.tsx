33962a4578d0643eec4569550e978e16df946471
alyamr96 <aliamrosman16@gmail.com>
2022-10-05 09:46:04 +0200

    Implement Features Section

---
+++ src/app/components/features/Features.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import FImage1 from "../../../images/f-img-1.jpg";
+import Icon1 from "../../../images/icon-1.png";
+import FImage2 from "../../../images/f-img-2.jpg";
+import Icon2 from "../../../images/icon-2.png";
+import FImage3 from "../../../images/f-img-3.jpg";
+import Icon3 from "../../../images/icon-3.png";
+import "./Features.css";
+
+export default function Features() {
+  return (
+    <section className="features" id="features">
+      <h1 className="heading">
+        {" "}
+        <span>gym features</span>{" "}
+      </h1>
+      <div className="box-container">
+        <div className="box">
+          <div className="image">
+            <img src={FImage1} alt="" />
+          </div>
+          <div className="content">
+            <img src={Icon1} alt="" />
+            <h3>body building</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Similique, atque.
+            </p>
+            <a href="#" className="btn">
+              read more
+            </a>
+          </div>
+        </div>
+        <div className="box second">
+          <div className="image">
+            <img src={FImage2} alt="" />
+          </div>
+          <div className="content">
+            <img src={Icon2} alt="" />
+            <h3>gym for men</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Similique, atque.
+            </p>
+            <a href="#" className="btn">
+              read more
+            </a>
+          </div>
+        </div>
+        <div className="box">
+          <div className="image">
+            <img src={FImage3} alt="" />
+          </div>
+          <div className="content">
+            <img src={Icon3} alt="" />
+            <h3>gym for women</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Similique, atque.
+            </p>
+            <a href="#" className="btn">
+              read more
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
