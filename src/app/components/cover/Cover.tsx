6f455b8b9e4cd7a6bb09fde78aefff8792646af5
alyamr96 <aliamrosman16@gmail.com>
2022-10-10 20:31:53 +0200

    implement cover

--- src/app/components/cover/Cover.tsx
+++ src/app/components/cover/Cover.tsx
@@ -4,18 +4,26 @@ import Verified from "../../../images/verified.jpg";
 
 export default function Cover() {
   return (
-    <div className="header">
-      <div className="logo-slogan">
-        <div className="icon">
-          <img src={Verified} alt="verified-icon" className="verified-icon" />
-        </div>
-        <div className="slogan">
-          <span>#AMMARIFIED</span>
-        </div>
-        <div className="slogan-content">
-          <span>DARE, TO TRANSFORM</span>
+    <section className="home" id="home">
+      <div className="slide">
+        <div className="content">
+          <div className="logo-slogan">
+            <div className="icon">
+              <img
+                src={Verified}
+                alt="verified-icon"
+                className="verified-icon"
+              />
+            </div>
+            <div className="slogan">
+              <span>#AMMARIFIED</span>
+            </div>
+            <div className="slogan-content">
+              <span>DARE, TO TRANSFORM</span>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
